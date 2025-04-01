6d17d8dfe3cfe58fcd96c94a18462890b808dece
NKVDream <Kmv235@yandex.ru>
2025-04-01 04:38:38 -0700

    1 done

---
+++ dpm-6/dpm-6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Task1
+{
+    static void Main()
+    {
+        int[] mas = {1, 1, 2, 2, 3, 4, 4, 4, 5, 5};
+        int[] mas2 = mas.Distinct().ToArray();
+        foreach (int i in mas2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
