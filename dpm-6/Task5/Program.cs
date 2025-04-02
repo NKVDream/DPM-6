0810e4973a2dd032387e8367c3836ddcce0e6e42
NKVDream <Kmv235@yandex.ru>
2025-04-02 01:52:42 -0700

    5 done

---
+++ dpm-6/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Task5
+{
+    static void Main()
+    {
+        Console.Write("write down any word: ");string word = Console.ReadLine();
+        Task5.star(word);
+    }
+    static void star(string word)
+    {
+        int count = 0;
+        foreach (char item in word)
+        {
+            count++;
+        }
+        Console.WriteLine(count);
+        for (int i = 0; i < count; count--)
+        {
+            word += "*";
+        }
+        Console.WriteLine(word);
+    }
+}
--- dpm-6/dpm-6/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ dpm-6/dpm-6/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdeb4796de01b1e96d85e4a1663fc7bcbe8e7c12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b984ac0d56b9ffdc8db442aa89ddcf3be06bb1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
