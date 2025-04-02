fdeb4796de01b1e96d85e4a1663fc7bcbe8e7c12
NKVDream <Kmv235@yandex.ru>
2025-04-02 01:36:29 -0700

    3 done

---
+++ dpm-6/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Task3
+{
+    static void Main()
+    {
+        Console.WriteLine("write anything");string word = Console.ReadLine();
+        Task3.check(word);
+    }
+    static bool check(string word)
+    {
+        if (word[0] == word[word.Length-1])
+        {
+            Console.WriteLine("first and last letter are equal");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("first and last letters are not equal");
+            return false;
+        }
+    }
+}
--- dpm-6/dpm-6/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ dpm-6/dpm-6/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d17d8dfe3cfe58fcd96c94a18462890b808dece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67c7ca7d04a89a07199beff368b2db67631cd9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
