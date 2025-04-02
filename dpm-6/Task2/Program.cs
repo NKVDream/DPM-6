f67c7ca7d04a89a07199beff368b2db67631cd9c
NKVDream <Kmv235@yandex.ru>
2025-04-02 01:19:57 -0700

    2 done

---
+++ dpm-6/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Task2
+{
+    static void Main()
+    {
+        Console.Write("write down anything: ");string a = Console.ReadLine();
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(a);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(a);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(a);
+    }
+}
