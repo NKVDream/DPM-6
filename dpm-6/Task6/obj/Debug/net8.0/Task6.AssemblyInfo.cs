43d916b01ad8755f14a6f0de546fb97bcd566797
NKVDream <Kmv235@yandex.ru>
2025-04-02 02:04:29 -0700

    7 done

--- dpm-6/Task6/obj/Debug/net8.0/Task6.AssemblyInfo.cs
+++ dpm-6/Task6/obj/Debug/net8.0/Task6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0810e4973a2dd032387e8367c3836ddcce0e6e42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f78c93685bab9f02e74217aade1615f7b89f814")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ dpm-6/Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Task7
+{
+    static void Main()
+    {
+        Console.Write("write down anything: "); string word = Console.ReadLine();
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(word);
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(word);
+        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(word);
+        Console.ResetColor();
+    }
+}
